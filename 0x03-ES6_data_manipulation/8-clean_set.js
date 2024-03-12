62c51aa9cedac8f61a8b2fcb2f25c2007e3d3595
Mugisha Goal <degaullemugisha@gmail.com>
2024-03-12 21:00:55 +0000

    8-main.js

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+function cleanSet(set, startString) {
+  // Filter values in the set that start with the specified string
+  const filteredValues = Array.from(set).filter(value => value.startsWith(startString));
+
+  // Join the filtered values and separate them with '-'
+  return filteredValues.join('-');
+}
+
+export default cleanSet;
