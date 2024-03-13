0906c9720f1ade395400ddf877207b23807f7ede
Mugisha Goal <degaullemugisha@gmail.com>
2024-03-13 11:51:31 +0000

    returns a string of all the set values that start with a specific string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 function cleanSet(set, startString) {
   // Filter values in the set that start with the specified string
-  const filteredValues = Array.from(set).filter(value => value.startsWith(startString));
+  const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
 
   // Join the filtered values and separate them with '-'
   return filteredValues.join('-');
