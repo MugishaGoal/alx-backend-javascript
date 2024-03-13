ad696ccf0354cee0b571f2fc17adcd9535671518
Mugisha Goal <degaullemugisha@gmail.com>
2024-03-13 11:41:37 +0000

    returns a new ArrayBuffer with an Int8 value at a specific position

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,7 @@
 function createInt8TypedArray(length, position, value) {
   // Check if the position is within the range of the array length
   if (position < 0 || position >= length) {
-    throw new Error("Position outside range");
+    throw new Error('Position outside range');
   }
 
   // Create a new ArrayBuffer with the specified length
