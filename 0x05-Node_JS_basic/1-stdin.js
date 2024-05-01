1ef75a3c252213f5d9e0f3979001fc500a90e0d1
Mugisha Goal <degaullemugisha@gmail.com>
2024-05-01 21:06:36 +0000

    A function that will be executed through command line

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,13 @@
-// Display initial message
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Listen for user input on stdin
-process.stdin.once('data', (input) => {
-    // Remove newline character from input and display user's name
-    const name = input.toString().trim();
-    process.stdout.write(`Your name is: ${name}\n`);
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
 
-    // Display closing message
-    process.stdout.write("This important software is now closing\n");
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
 
-    // Exit the program
-    process.exit(0);
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
